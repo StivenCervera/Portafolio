16243a20221dd67339d9fb90f6a442e613a1170d
Stiven <matcer2004@gmail.com>
2021-09-26 22:21:07 -0500

    Ruta 6

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -17,13 +17,13 @@ const Router = () => {
         
         <Switch>
 
-        <Route exact path="/Portafolio" component={Home} />
+        <Route exact path="/" component={Home} />
 
-        <Route exact path="perfil" component={Perfil} />
+        <Route exact path="/perfil" component={Perfil} />
 
-        <Route exact path="estudios" component={Estudios} />
+        <Route exact path="/estudios" component={Estudios} />
 
-        <Route exact path="experiencia" component={Experiencia} />
+        <Route exact path="/experiencia" component={Experiencia} />
 
         <Route>
             <Error404 />
